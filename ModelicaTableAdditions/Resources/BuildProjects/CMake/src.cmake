6a383ae2c3f8c587b583c2263b42b720cfe8fc56
tbeu <tc@tbeu.de>
2024-04-05 21:27:13 +0200

    Provide ModelicaMatIO for unit tests (and avoid defining NO_MATIO)

--- ModelicaTableAdditions/Resources/BuildProjects/CMake/src.cmake
+++ ModelicaTableAdditions/Resources/BuildProjects/CMake/src.cmake
@@ -73,9 +73,6 @@ endif()
 if(MODELICA_DUMMY_FUNCTION_USERTAB OR BUILD_TESTING)
   target_compile_definitions(ModelicaTableAdditions PRIVATE -DDUMMY_FUNCTION_USERTAB=1)
 endif()
-if(BUILD_TESTING)
-  target_compile_definitions(ModelicaIOAdditions PRIVATE -DNO_MATIO=1)
-endif()
 
 install(
   TARGETS ModelicaTableAdditions ModelicaIOAdditions parson
--- ModelicaTableAdditions/Resources/BuildProjects/CMake/test.cmake
+++ ModelicaTableAdditions/Resources/BuildProjects/CMake/test.cmake
@@ -22,6 +22,42 @@ FetchContent_MakeAvailable(googletest)
 
 set(MODELICA_TABLE_ADDITIONS_TEST_DIR "${MODELICA_TABLE_ADDITIONS_RESOURCES_DIR}/Test")
 if(EXISTS "${MODELICA_TABLE_ADDITIONS_TEST_DIR}")
+  set(MSL_URL https://raw.githubusercontent.com/modelica/ModelicaStandardLibrary/master)
+  if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon")
+    file(DOWNLOAD
+      "${MSL_URL}/.CI/Test/Common.c"
+      "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/Common.c")
+    file(DOWNLOAD
+      "${MSL_URL}/Modelica/Resources/C-Sources/ModelicaMatIO.c"
+      "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/ModelicaMatIO.c")
+    file(DOWNLOAD
+      "${MSL_URL}/Modelica/Resources/C-Sources/ModelicaMatIO.h"
+      "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/ModelicaMatIO.h")
+    file(DOWNLOAD
+      "${MSL_URL}/Modelica/Resources/C-Sources/read_data_impl.h"
+      "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/read_data_impl.h")
+    file(DOWNLOAD
+      "${MSL_URL}/Modelica/Resources/C-Sources/safe-math.h"
+      "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/safe-math.h")
+    file(DOWNLOAD
+      "${MSL_URL}/Modelica/Resources/C-Sources/snprintf.c"
+      "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/snprintf.c")
+  endif()
+  set(TEST_COMMON_SOURCES
+    "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/Common.c"
+    "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/ModelicaMatIO.c"
+    "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/ModelicaMatIO.h"
+    "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/read_data_impl.h"
+    "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/safe-math.h"
+    "${CMAKE_CURRENT_BINARY_DIR}/ModelicaTableAdditionsTestCommon/snprintf.c"
+    "${MODELICA_UTILITIES_INCLUDE_DIR}/ModelicaUtilities.h"
+  )
+  add_library(ModelicaTableAdditionsTestCommon STATIC ${TEST_COMMON_SOURCES})
+  set_target_properties(ModelicaTableAdditionsTestCommon PROPERTIES FOLDER "Test")
+  if(MSVC)
+    target_compile_options(ModelicaTableAdditionsTestCommon PRIVATE /wd4267)
+  endif()
+
   set(MODELICA_TESTS
     Tables
     TablesFromCsvFile
@@ -31,7 +67,6 @@ if(EXISTS "${MODELICA_TABLE_ADDITIONS_TEST_DIR}")
   foreach(TEST ${MODELICA_TESTS})
     add_executable(Test${TEST}
       "${MODELICA_TABLE_ADDITIONS_TEST_DIR}/${TEST}.cc"
-      "${MODELICA_TABLE_ADDITIONS_TEST_DIR}/Common.cc"
       "${MODELICA_TABLE_ADDITIONS_TEST_DIR}/Constants.h"
     )
     target_compile_features(Test${TEST} PRIVATE cxx_std_17)
@@ -39,6 +74,7 @@ if(EXISTS "${MODELICA_TABLE_ADDITIONS_TEST_DIR}")
     target_link_libraries(Test${TEST} PRIVATE
       ModelicaTableAdditions
       ModelicaIOAdditions
+      ModelicaTableAdditionsTestCommon
       parson
       gtest
     )
