b6dc06ce463e473465dbbc1bb2cb60a4d579ba2e
tbeu <tc@tbeu.de>
2025-04-15 21:51:38 +0200

    Update gconstructor handling
    * Introduce and apply name mangling to avoid MSVC duplicated symbols issue
    * Bump version by renaming file and macros

--- ModelicaTableAdditions/Resources/C-Sources/ModelicaTableAdditions.c
+++ ModelicaTableAdditions/Resources/C-Sources/ModelicaTableAdditions.c
@@ -178,7 +178,7 @@
 #define uthash_strlen(s) key_strlen(s)
 #define HASH_NONFATAL_OOM 1
 #include "uthash.h"
-#include "gconstructor.h"
+#include "g2constructor.h"
 #endif
 #include <assert.h>
 #include <float.h>
@@ -418,16 +418,16 @@ typedef struct TableShare {
 static TableShare* tableShare = NULL;
 #if defined(_POSIX_) && !defined(NO_MUTEX)
 #include <pthread.h>
-#if defined(G_HAS_CONSTRUCTORS)
+#if defined(G2_HAS_CONSTRUCTORS)
 static pthread_mutex_t m;
-G_DEFINE_CONSTRUCTOR(initializeMutex)
-static void initializeMutex(void) {
+G2_DEFINE_CONSTRUCTOR(G2_FUNCNAME(initializeMutex))
+static void G2_FUNCNAME(initializeMutex)(void) {
     if (pthread_mutex_init(&m, NULL) != 0) {
         ModelicaError("Initialization of mutex failed\n");
     }
 }
-G_DEFINE_DESTRUCTOR(destroyMutex)
-static void destroyMutex(void) {
+G2_DEFINE_DESTRUCTOR(G2_FUNCNAME(destroyMutex))
+static void G2_FUNCNAME(destroyMutex)(void) {
     if (pthread_mutex_destroy(&m) != 0) {
         ModelicaError("Destruction of mutex failed\n");
     }
@@ -437,24 +437,24 @@ static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 #endif
 #define MUTEX_LOCK() pthread_mutex_lock(&m)
 #define MUTEX_UNLOCK() pthread_mutex_unlock(&m)
-#elif defined(_WIN32) && defined(G_HAS_CONSTRUCTORS)
+#elif defined(_WIN32) && defined(G2_HAS_CONSTRUCTORS)
 #if !defined(WIN32_LEAN_AND_MEAN)
 #define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
 static CRITICAL_SECTION cs;
-#ifdef G_DEFINE_CONSTRUCTOR_NEEDS_PRAGMA
-#pragma G_DEFINE_CONSTRUCTOR_PRAGMA_ARGS(ModelicaTableAdditions_initializeCS)
+#ifdef G2_DEFINE_CONSTRUCTOR_NEEDS_PRAGMA
+#pragma G2_DEFINE_CONSTRUCTOR_PRAGMA_ARGS(G2_FUNCNAME(ModelicaTableAdditions_initializeCS))
 #endif
-G_DEFINE_CONSTRUCTOR(ModelicaTableAdditions_initializeCS)
-static void ModelicaTableAdditions_initializeCS(void) {
+G2_DEFINE_CONSTRUCTOR(G2_FUNCNAME(ModelicaTableAdditions_initializeCS))
+static void G2_FUNCNAME(ModelicaTableAdditions_initializeCS)(void) {
     InitializeCriticalSection(&cs);
 }
-#ifdef G_DEFINE_DESTRUCTOR_NEEDS_PRAGMA
-#pragma G_DEFINE_DESTRUCTOR_PRAGMA_ARGS(ModelicaTableAdditions_deleteCS)
+#ifdef G2_DEFINE_DESTRUCTOR_NEEDS_PRAGMA
+#pragma G2_DEFINE_DESTRUCTOR_PRAGMA_ARGS(G2_FUNCNAME(ModelicaTableAdditions_deleteCS))
 #endif
-G_DEFINE_DESTRUCTOR(ModelicaTableAdditions_deleteCS)
-static void ModelicaTableAdditions_deleteCS(void) {
+G2_DEFINE_DESTRUCTOR(G2_FUNCNAME(ModelicaTableAdditions_deleteCS))
+static void G2_FUNCNAME(ModelicaTableAdditions_deleteCS)(void) {
     DeleteCriticalSection(&cs);
 }
 #define MUTEX_LOCK() EnterCriticalSection(&cs)
